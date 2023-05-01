51254f3183f62588da328a4f3e3e4b87e230ef0d
ebronson68 <ebronson@amuniversal.com>
2023-05-01 16:02:56 -0500

    key vault script updates

--- Get-Secrets.ps1
+++ Get-Secrets.ps1
@@ -1,4 +1,5 @@
 param (
+    [string]$TenantName = "Andrews McMeel Universal",
     [string]$KeyVaultName,
     [string]$File = '.env',
     [string]$RepositoryName = ((git remote get-url origin).Split("/")[-1].Replace(".git","")),
@@ -11,11 +12,21 @@ if (!(Get-Module -ListAvailable Az.KeyVault)) {
     Install-Module -Name Az.KeyVault -Confirm:$false
 }
 
-Clear-Content -Path $File -ErrorAction SilentlyContinue
+# Switch to AMU Tenant
+Get-AzTenant | ForEach-Object {
+    # Search for tenant ID that has a name matching $TenantName
+    if ($_.Name | Select-String $TenantName) {
+        $TenantId = Set-AzContext -TenantId $_.Id
+    }
+}
 
-if (!"$KeyVaultName") {
+Clear-Content -Path "${File}.tmp" -ErrorAction SilentlyContinue
+
+# If $KeyVaultName argument isn't set
+if (!$PSBoundParameters.ContainsKey('KeyVaultName')) {
+    # If $Environment argument isn't set, use default value
     if (!$PSBoundParameters.ContainsKey('Environment')) {
-        Write-Host "Environment missing. Defaulting to development." -ForegroundColor DarkGray
+        Write-Host "Environment missing. Defaulting to $Environment." -ForegroundColor DarkGray
     }
 
     Write-Host "Searching for key vault with tags: 'repository-name=$RepositoryName;environment=$Environment'" -ForegroundColor DarkGray
@@ -25,18 +36,19 @@ else {
     Write-Host "Searching for key vault named: $KeyVaultName" -ForegroundColor DarkGray
 }
 
-$Secrets = (Get-AzKeyVaultSecret -VaultName $KeyVaultName).Name
+# Get secrets for the key vault
+$Secrets = (Get-AzKeyVaultSecret -VaultName "$KeyVaultName").Name
+Write-Host "Key vault found: $KeyVaultName" -ForegroundColor DarkGray
+Write-Host "Retrieving secrets..." -ForegroundColor DarkGray
 
-if ($Secrets) {
-    Write-Host "Key vault found: $KeyVaultName" -ForegroundColor DarkGray
-    Write-Host "Retrieving secrets..." -ForegroundColor DarkGray
-}
+# Loop through secrets and add them to ${File}.tmp
 $Secrets | ForEach-Object {
     $SecretName = $_.ToUpper().Replace("-", "_").Replace("`"", "")
-    $SecretValue = (Get-AzKeyVaultSecret -VaultName $KeyVaultName -Name $_).SecretValue | ConvertFrom-SecureString -AsPlainText
-    $Secret = $SecretName + "=" + $SecretValue
+    $SecretValue = (Get-AzKeyVaultSecret -VaultName "$KeyVaultName" -Name $_).SecretValue | ConvertFrom-SecureString -AsPlainText
+    $Secret = "$SecretName" + "=" + "$SecretValue"
     Write-Host "$Secret"
-    Add-Content -Path $File -Value $Secret
+    Add-Content -Path "${File}.tmp" -Value "$Secret"
 }
 
+Copy-Item -Path "${File}.tmp" -Destination "${File}"
 Write-Host "✨ .env file generated from $KeyVaultName" -ForegroundColor Green
